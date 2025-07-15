1dada27d60b16e6d895b10ed5459ca6e25fb9b5f
ricoputrabuana <149350841+ricoputrabuana@users.noreply.github.com>
2025-07-15 22:23:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,5 +41,4 @@ def index():
     return "Proxy is running", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
